cfc465bb6302ce2808e7700099604836edcb6bac
Cyrille <cypierre58@gmail.com>
2018-06-16 23:04:28 +0200

    [cam follow] Quick test of ball following

--- src/gamescene.cpp
+++ src/gamescene.cpp
@@ -8,13 +8,23 @@ using namespace cocos2d;
 bool GameScene::init() {
 	if(!Scene::init()) return false;
 
+	// the root node of all game elements
+	auto field = Node::create();
+	field->setContentSize(getContentSize());
+	addChild(field);
+
 	auto background = Factory<Background>::create({1000, 600}, 50);
 	background->setPositionNormalized({.5, .5});
-	addChild(background);
+	field->addChild(background);
 
 	auto playerball = Factory<PlayerBall>::create(10, {.8, .7, .2, 1});
 	playerball->setPositionNormalized({.5, .5});
-	addChild(playerball);
+	field->addChild(playerball);
+
+	field->schedule([this, field, playerball] (float dt) {
+		static Vec2 const halfSize = this->getContentSize() / 2;
+		field->setPosition(halfSize - playerball->getPosition());
+	}, "camera_center");
 
 	return true;
 }
