e9225ca2ec8a3f571e1295be8fa97f26cbcfe147
Cyrille <cypierre58@gmail.com>
2018-06-17 19:11:27 +0200

    [food] Wave scale

--- src/foodgenerator.cpp
+++ src/foodgenerator.cpp
@@ -1,4 +1,5 @@
 #include <2d/CCDrawNode.h>
+#include <cocos2d.h>
 #include <base/ccRandom.h>
 #include "foodgenerator.hpp"
 
@@ -7,7 +8,7 @@ using namespace cocos2d;
 bool FoodGenerator::init(Node * foods) {
 	schedule([this] (float) {
 		Vec2 spawnSize = getParent()->getContentSize() / 2 - Size{15, 15};
-		float radius = random(2.f, 10.f);
+		float radius = random(4.f, 10.f);
 		Vec2 pos{random(-spawnSize.x, spawnSize.x), random(-spawnSize.y, spawnSize.y)};
 		Color4F color{rand_0_1(), rand_0_1(), rand_0_1(), 1};
 
@@ -16,6 +17,13 @@ bool FoodGenerator::init(Node * foods) {
 		food->drawDot(Vec2::ZERO, radius, color);
 		food->setPosition(pos);
 		getParent()->getChildByName("foods")->addChild(food);
+
+		// wave scale action
+		float time = random(.7f, 2.f);
+		food->runAction(RepeatForever::create(Sequence::create(
+			EaseSineInOut::create(ScaleTo::create(time, 1.2f)),
+			EaseSineInOut::create(ScaleTo::create(time, .8f)), nullptr)));
+		
 	}, .1, "gen");
 
 }
